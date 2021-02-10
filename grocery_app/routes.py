d1241a53e1d5fe8c57ac3aedb678229d2a690631
johnnyquinn <jftq97@gmail.com>
2021-02-09 19:48:54 -0500

    filled out new_item route

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -37,7 +37,7 @@ def new_store():
         db.session.add(new_store)
         db.session.commit()
 
-        flash('New grocery store successly added')
+        flash('New grocery store successfully added')
         return redirect(url_for('main.store_detail', store_id=new_store.id))
 
     # TODO: Send the form to the template and use it to render the form fields
@@ -46,14 +46,25 @@ def new_store():
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
     # TODO: Create a GroceryItemForm
+    form = GroceryItemForm()
 
     # TODO: If form was submitted and was valid:
     # - create a new GroceryItem object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the item detail page.
-
+    if form.validate_on_submit():
+        new_item = GroceryItem(
+            name = form.name.data,
+            price = form.price.data,
+            category = form.category.data,
+            photo_url = form.photo_url.data,
+            store = form.store.data
+        )
+        
+        flash('New grocery item successfully added')
+        return redirect(url_for('main.item_detail', item_id=new_item.id))
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_item.html')
+    return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 def store_detail(store_id):
