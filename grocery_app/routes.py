687d7bc51f13af0c6e5047bc838c3a37c42278e1
johnnyquinn <jftq97@gmail.com>
2021-02-25 14:28:08 -0500

    added login/signup forms, added signup/login/logout routes, added login/signup/logout buttons to base.html, and added login_required decorator to routes

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
-from grocery_app.models import GroceryStore, GroceryItem
-from grocery_app.forms import GroceryItemForm, GroceryStoreForm
-# from grocery_app.forms import BookForm, AuthorForm, GenreForm
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app.forms import GroceryItemForm, GroceryStoreForm, SignUpForm, LoginForm
+from flask_login import login_required, LoginManager
+from flask_bcrypt import Bcrypt
+
 
 # Import app and db from events_app package so that we can run app
 from grocery_app import app, db
@@ -20,6 +22,7 @@ def homepage():
     return render_template('home.html', all_stores=all_stores)
 
 @main.route('/new_store', methods=['GET', 'POST'])
+@login_required
 def new_store():
     # TODO: Create a GroceryStoreForm
     form = GroceryStoreForm()
@@ -43,6 +46,7 @@ def new_store():
     return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
+@login_required
 def new_item():
     # TODO: Create a GroceryItemForm
     form = GroceryItemForm()
@@ -59,6 +63,8 @@ def new_item():
             photo_url = form.photo_url.data,
             store = form.store.data
         )
+        db.session.add(new_item)
+        db.session.commit()
         
         flash('New grocery item successfully added')
         return redirect(url_for('main.item_detail', item_id=new_item.id))
@@ -66,6 +72,7 @@ def new_item():
     return render_template('new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
+@login_required
 def store_detail(store_id):
     store = GroceryStore.query.get(store_id)
     # TODO: Create a GroceryStoreForm and pass in `obj=store`
@@ -91,6 +98,7 @@ def store_detail(store_id):
     return render_template('store_detail.html', store=store, form=form)
 
 @main.route('/item/<item_id>', methods=['GET', 'POST'])
+@login_required
 def item_detail(item_id):
     item = GroceryItem.query.get(item_id)
     # TODO: Create a GroceryItemForm and pass in `obj=item`
@@ -116,4 +124,44 @@ def item_detail(item_id):
     # TODO: Send the form to the template and use it to render the form fields
     item = GroceryItem.query.get(item_id)
     return render_template('item_detail.html', item=item, form=form)
+
+# routes.py
+
+auth = Blueprint("auth", __name__)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print('in signup')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        print('created')
+        return redirect(url_for('auth.login'))
+    print(form.errors)
+    return render_template('signup.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
 
